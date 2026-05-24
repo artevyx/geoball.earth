4b320976701235a6290e95b734a183d4b5cbcabf
artevyx <artevyx@tutanota.com>
2026-05-24 11:25:29 -0700

    updated ref aliases

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   reactCompiler: true,
+  allowedDevOrigins: ['nonboiling-lorilee-manageable.ngrok-free.dev'],
 };
 
 export default nextConfig;
